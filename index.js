d2d706733aac12a09239fcdbcd5f0b8d85ddfd7f
russell-loube-optimizely <russell.loube@optimizely.com>
2023-12-04 17:04:13 -0600

    3.13: Phonebook database, step1 (post endpoint not yet implemented)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const Person = require("./models/person");
 app.use(express.json());
 const morgan = require("morgan");
 morgan.token("body", (req, res) => JSON.stringify(req.body));
@@ -10,59 +12,64 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.static("build"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/info", (req, res) => {
-  const peopleCount = persons.length;
-  const currentDate = new Date();
-  res.send(
-    `<p>Phone has info for ${peopleCount} people</p>
-    <p>${currentDate}</p>`
-  );
+  Person.find({}).then((persons) => {
+    res.send(
+      `<p>Phone has info for ${persons.length} people</p>
+      <p>${new Date()}</p>`
+    );
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => {
+      console.log(`Error getting person: ${error}}`);
+      return res.status(400).json({
+        error: "person not found",
+      });
+    });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
+  const id = req.params.id;
   console.log("id:", id, typeof id);
-  persons = persons.filter((person) => person.id !== id);
-  console.log("phonebook:", persons);
-  res.status(204).end();
+  Person.deleteOne({ _id: id }).then((result) => {
+    console.log("result:", result);
+    res.status(204).end();
+  });
 });
 
 app.post("/api/persons", (req, res) => {
