77accee2421450186fc36c03b7e7cb2c54bc8dbe
russell-loube-optimizely <russell.loube@optimizely.com>
2023-12-15 09:31:28 -0600

    moved error handling to middleware

--- index.js
+++ index.js
@@ -12,65 +12,101 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.static("build"));
 
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/info", (req, res) => {
-  Person.find({}).then((persons) => {
-    res.send(
-      `<p>Phone has info for ${persons.length} people</p>
+app.get("/info", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.send(
+        `<p>Phone has info for ${persons.length} people</p>
       <p>${new Date()}</p>`
-    );
-  });
+      );
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       res.json(person);
     })
-    .catch((error) => {
-      console.log(`Error getting person: ${error}}`);
-      return res.status(400).json({
-        error: "person not found",
-      });
-    });
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("id:", id, typeof id);
-  Person.deleteOne({ _id: id }).then((result) => {
-    console.log("result:", result);
-    res.status(204).end();
-  });
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+// Middleware for validating request body
+function validatePerson(req, res, next) {
   const { name, number } = req.body;
 
   if (!name || !number) {
     return res.status(400).json({ error: "Name and number are required" });
   }
 
-  Person.findOne({ name }).then((existingPerson) => {
-    if (existingPerson) {
-      return res.status(400).json({ error: "Person already exists" });
-    }
+  Person.findOne({ name })
+    .then((existingPerson) => {
+      if (existingPerson) {
+        return res.status(400).json({ error: "Person already exists" });
+      }
+      next();
+    })
+    .catch((error) => next(error));
+}
 
-    const newPerson = new Person({
-      name,
-      number,
-    });
+app.post("/api/persons", validatePerson, (req, res, next) => {
+  const { name, number } = req.body;
 
-    newPerson.save().then((savedPerson) => {
-      res.json(savedPerson);
-    });
+  const newPerson = new Person({
+    name,
+    number,
   });
+
+  newPerson
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  console.log("res:", res.body);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = 3001;
 app.listen(PORT, () => {
